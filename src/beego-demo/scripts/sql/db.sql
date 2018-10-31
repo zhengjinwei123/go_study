6ace67f5399506719c6205c92924f06acc67ca99
zhengjinwei <zhengjinwei123>
2018-10-31 12:14:20 +0800

    delete beego-demo dir

--- src/beego-demo/scripts/sql/db.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE beego_demo;
-USE beego_demo;
-CREATE TABLE roles (
-  id		BIGINT PRIMARY KEY,
-  name		VARCHAR(255),
-  password	VARCHAR(255),
-  reg_date	DATETIME
-);
-INSERT INTO roles VALUES(1, 'admin', 'admin', now());
