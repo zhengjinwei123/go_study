24baf0195150f033ea4cace2228434d343f716b9
zhengjinwei <zhengjinwei123>
2018-11-01 18:39:25 +0800

    add captor

--- src/newsCaptorsTask/global/configManager.go
+++ src/newsCaptorsTask/global/configManager.go
@@ -1,3 +1,22 @@
 package global
 
+import (
+	"newsCaptorsTask/util/fileParser"
+	"newsCaptorsTask/util"
+)
+
+var C *fileParser.Config
+
+func init(){
+	p,err := util.GetFilePath("../conf/app.ini")
+	if err != nil {
+		panic(err)
+	}
+	C = fileParser.SetConfig(p)
+}
+
+func AppConf() *fileParser.Config {
+	return C
+}
+
 
--- src/newsCaptorsTask/main.go
+++ src/newsCaptorsTask/main.go
@@ -1,28 +1,14 @@
 package main
 
 import (
-	"newsCaptorsTask/util/fileParser"
 	"fmt"
-	"runtime"
-	"path/filepath"
-	"newsCaptorsTask/util"
+	"newsCaptorsTask/global"
 )
 
-func GetCurDir() string {
-	_, file, _, ok := runtime.Caller(1)
-	if !ok {
-		panic("11111")
-	}
-	return filepath.Dir(file)
-}
+
 
 func main() {
-	_,err := util.GetFilePath("./conf/app.ini")
-	if err != nil {
-		panic(err)
-	}
-	conf := fileParser.SetConfig(GetCurDir() + "/conf/app.ini")
-
+	conf := global.AppConf()
 	fmt.Println(conf.GetValue("mysql","port"))
 	fmt.Println(conf.GetValue("base","appName"))
 	fmt.Println(conf.GetValue("mysql","host"))
