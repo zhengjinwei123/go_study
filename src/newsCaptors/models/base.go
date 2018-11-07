53b250c3fcda130c3bb3249f0171cfdf9736b579
zhengjinwei <zhengjinwei123>
2018-11-07 18:40:52 +0800

    update captor

--- src/newsCaptors/models/base.go
+++ src/newsCaptors/models/base.go
@@ -4,16 +4,15 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/astaxie/beego/orm"
 	"github.com/astaxie/beego"
-	"fmt"
 )
 
 
 func Init() {
-	host := beego.AppConfig.String("dbhost")
-	port := beego.AppConfig.String("dbport")
-	user := beego.AppConfig.String("dbuser")
-	password := beego.AppConfig.String("dbpassword")
-	database := beego.AppConfig.String("dbdatabase")
+	host := beego.AppConfig.String("mysql::host")
+	port := beego.AppConfig.String("mysql::port")
+	user := beego.AppConfig.String("mysql::user")
+	password := beego.AppConfig.String("mysql::password")
+	database := beego.AppConfig.String("mysql::database")
 
 	orm.RegisterDriver("mysql",orm.DRMySQL)
 	url := user + ":" + password + "@tcp("+host+":"+port+")/"+database+"?charset=utf8&parseTime=True&loc=Local"
