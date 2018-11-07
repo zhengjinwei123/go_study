2f13c637cebe19c2f04f9a843e102158373130f6
zhengjinwei <zhengjinwei123>
2018-11-07 11:02:49 +0800

    update captor

--- src/newsCaptorsTask/data/db.sql
+++ src/newsCaptorsTask/data/db.sql
@@ -14,10 +14,11 @@ CREATE TABLE `t_captorinfo` (
 
 CREATE TABLE `t_hobby` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `hobby` varchar(255) not null default '' comment '爱好',
-  `type` tinyint(3) not null default 0 comment '分类',
+  `hobby` varchar(255) NOT NULL DEFAULT '' COMMENT '爱好',
+  `type` tinyint(3) NOT NULL DEFAULT '0' COMMENT '分类',
   `createAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updateAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `unq_hobby_type` (`hobby`,`type`),
   KEY `idx_type` (`type`)
-)ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
